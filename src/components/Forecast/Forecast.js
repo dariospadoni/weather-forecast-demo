b9efd0fbcf9b5a3402448807ad362e0229439eb6
Dario Spadoni <dariospadoni@gmail.com>
2020-10-21 11:16:02 +0200

    BEM and scss for Forecast component

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
-import './Forecast.css';
+import './Forecast.scss';
 import getIcon from '../Icons';
 import { toCelsius } from '../../utils';
 
 
 const Forecast = ({ selected, icon, date, temperature }) => 
-  <div className={`forecast ${selected ? 'active': ''}`}>
-    <div className="time">
+  <div className={`forecast ${selected ? 'forecast--active': ''}`}>
+    <div className="forecast__time">
       {moment(date).format('HH:mm')}
     </div>
-    <div className="condition">{getIcon(icon)}</div>
-    <div className="temperature">{toCelsius(temperature)}</div>
+    <div className="forecast__condition">{getIcon(icon)}</div>
+    <div className="forecast__temperature">{toCelsius(temperature)}</div>
   </div>;
+
 
 Forecast.propTypes = {
   selected: PropTypes.bool,
@@ -21,5 +22,6 @@ Forecast.propTypes = {
   temperature: PropTypes.number.isRequired,
   icon: PropTypes.string.isRequired,
 };
+
 
 export default Forecast;
